30dbbd6fcc5364220e2e1f193e40ed78bcb618e4
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-03-01 15:55:46 -0600

    passed all test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+// takes 2 arguments
+const repeatString = function (word, num) {
+    // create a variable to hold the sting that will be returned
+    let text = '';
+    
+    if (num < 0) return 'ERROR';
+    
+//create a loop that repeats the given number of times
+    for (i = 0; i < num; i++) {
+       // add the given string to the result on each loop 
+        text += word;
+    }
+    return text;
 };
 
 // Do not edit below this line
