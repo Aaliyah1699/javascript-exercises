de23bb30da2aaa95b6b8597c15503fcf2c7b9a03
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-03-01 17:04:30 -0600

    passed test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+// split it into something else first.. do the reversal and then join it back together into a string.
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
