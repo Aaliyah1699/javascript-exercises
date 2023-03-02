910d43a0c1a92499cfb38e34c617381b94fc7f7f
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-03-01 19:41:10 -0600

    rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+// Implement a function that takes an array and some other arguments then removes the other arguments from that array
+const removeFromArray = function (array, ...args) {
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        // so we create a new array with every item, except those that should be removed
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    // and return that array
+    return newArray;
 };
 
 // Do not edit below this line
