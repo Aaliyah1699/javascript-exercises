ca81913d3b2d8e7cdc4ed90d164beb3e77687e05
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-03-01 21:27:10 -0600

    originally passed 3 test  / 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+// use an `if` statement and `&&` to make sure all the conditions are met properly
+// divisible by 4, NOT 100 unless divisible by 400
+const leapYears = function (year) {
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
