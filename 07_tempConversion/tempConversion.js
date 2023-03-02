81880037589f8049526cb55f41aaebe209ee00fe
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-03-01 22:34:07 -0600

    finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // let celsius = (fahrenheit - 32) * (5/9);
+  // let round = Math.round(celsius * 10) / 10;
+  // return round;  Passed 4 test
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // let fahrenheit = (celsius * 5)/9 + 32;
+  // let rounded = Math.round(fahrenheit * 10) / 10;
+  // return rounded; passed 4 test
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
